e83eb5e63fd3253dd8c096c1f088f546605cda2d
maxx730 <max.kinghorn@gmail.com>
2020-03-11 08:23:49 -0700

    update

---
+++ views/Submit.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Submit extends React.Component {
+    render() {
+        return(
+            <View>
+                <Text>Submit</Text>
+            </View>
+        );
+    }
+}
+
+export default Submit;
