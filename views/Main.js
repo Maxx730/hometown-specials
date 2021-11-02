5007aec5c77eeac086f2f1c6bae8f40cd3a200b9
Maxx730 <max.kinghorn@gmail.com>
2021-11-02 11:54:37 -0400

    update

--- views/Main.js
+++ views/Main.js
@@ -60,37 +60,31 @@ class Main extends React.Component {
             toasting: false,
             toastMessage: '',
             prefs: null,
-            loading: true
+            loading: true,
+            errors: []
         }
     }
 
     //Grab the data from the backend server.
     componentDidMount() {
         _hasCachedData().then(proceed => {
+            // If we have found data then we want to read the cached data
             if (proceed) {
-                _getCachedData().then(data => {
-                    _getPrefs().then(prefs => {
-                        this.setState({
-                            prefs: prefs,
-                            loading: false,
-                            locations: data
-                        });
-                    });
-                });
+
             } else {
-                Network._getLocations().then(locations => {
-                    _saveCacheData(JSON.stringify({lastCached: new Date(), locations: locations})).then(() => {
-                        _getPrefs().then(prefs => {
-                            this.setState({
-                                prefs: prefs,
-                                loading: false,
-                                locations: locations
-                            });
-                        });
-                    });
-                });
+                // if the file does not yet exist we need to send a request to the server and get the data
+                
             }
-        })
+        }).catch(err => {
+            var _errors = JSON.parse(JSON.stringify(this.state.errors))
+            _errors.push({
+                title: 'ERROR',
+                desc: 'Unable to load cache file.'
+            });
+            this.setState({
+                errors: _errors
+            });
+        });
     }
 
 
@@ -98,174 +92,7 @@ class Main extends React.Component {
         return(
             <View behavior="padding" style={[Styles.Main]}>
                 {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-                {this.state.toasting && <Toast message={this.state.toastMessage} onComplete={() => {
-                    this.setState({
-                        toasting: false
-                    });
-                }}/>}
-
-                {(this.state.showModal && Platform.OS === 'android') && this._renderModal({
-                        title: 'Details',
-                        content: this._getModal(this.state.showAllInfo),
-                        onClose: () => {
-                            this.setState({
-                                focused: null,
-                                showModal: false,
-                                showHours: false,
-                                showAllInfo: false
-                            });
-                        }
-                })}
-                <View style={[Styles.Browse]}>
-                    <View style={[{
-                        elevation: 6,
-                        backgroundColor: '#FFFFFF',
-                        paddingBottom: 12,
-                        paddingTop: 24,
-                        justifyContent: 'flex-end',
-                        shadowColor: '#000',
-                        shadowOffset: { width: 0, height: 0 },
-                        shadowOpacity: 0.4,
-                        shadowRadius: 5,
-                    }]}>
-                        {this.state.loading && <Text>Loading!!!!</Text>}
-                        {this.state.location === 'list' ? <Head addPadding={true} day={this.state.day} setDay={this._setDay}/> : <View style={[Styles.SearchView]}>
-                            <Input icon={<AntDesign name={'search1'} size={24}/>} placeholder={`Search`} onChange={(value) => {
-                                this.setState({
-                                    searchTerm: value
-                                })
-                            }}/>
-                        </View>}
-                        <View style={[{
-                            flexDirection: 'row'
-                        }]}>
-                            <TouchableOpacity style={[Styles.TabIcon]} onPress={() => {
-                                this.props.navigation.navigate('Submit')
-                            }}>
-                                <Feather name={'edit'} size={32}/>
-                            </TouchableOpacity>
-                            <Tabs tabs={[{
-                                label: 'Today',
-                                callback: () => {
-                                    this.setState({
-                                        location: 'list'
-                                    });
-                                }
-                            },{
-                                label: 'Search',
-                                callback: () => {
-                                    this.setState({
-                                        location: 'search'
-                                    });
-                                }
-                            }]}/>
-                            <TouchableOpacity onPress={() => {
-                                this.props.navigation.navigate('Settings', {
-                                    prefs: this.state.prefs,
-                                    setDefaults: () => {
-                                        return new Promise(resolve => {
-                                            _setDefaults().then(prefs => {
-                                                resolve(prefs)
-                                            })
-                                        });
-                                    },
-                                    save: (prefs) => {
-                                        return new Promise(resolve => {
-                                            this._savePrefs(prefs).then(prefs => {
-                                                resolve(prefs)
-                                            });
-                                        });
-                                    },
-                                    refresh: () => {
-                                        this._requestData();
-                                    }
-                                });
-                            }} style={[Styles.TabIcon]}>
-                                <Feather name={'settings'} size={32}/>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    {this._renderNavigationLocation()}
-                    {(this.state.showModal && Platform.OS === 'ios') && this._renderModal({
-                        title: 'Details',
-                        content: this._getModal(this.state.showAllInfo),
-                        onClose: () => {
-                            this.setState({
-                                focused: null,
-                                showModal: false,
-                                showHours: false,
-                                showAllInfo: false
-                            });
-                        }
-                    })}
-                </View>
-            </View>
-        );
-    }
-
-    _getModal() {
-        return(
-            <View style={[{
-                flex: 1
-            }]}>
-                <View style={[Styles.ModalAdress]}>
-                    <View style={[{
-
-                    }]}>
-                        <Text style={[{
-                            fontWeight: 'bold',
-                            fontSize: 18
-                        }]}>{this.state.focused.name}</Text>
-                        <Text>{this.state.focused.location.street}, {this.state.focused.location.city}</Text>
-                    </View>
-                    <View style={{
-                        flexDirection: 'row'
-                    }}>
-                        {this.state.focused.delivering && <Feather size={24} name={'truck'}/>}
-                        {this.state.focused.takeout && <Feather size={24} name={'shopping-bag'}/>}
-                    </View>
-                </View>
-                <View style={[Styles.ModalTabs],{
-                    flexDirection: 'row',
-                    paddingTop: 16
-                }}>
-                    <View style={[Styles.TabIcon]}>
-
-                    </View>
-                    <Tabs tabs={[{
-                        label: 'Specials',
-                        callback: () => {
-                            this.setState({
-                                showHours: false
-                            });
-                        }
-                    },{
-                        label: 'Hours',
-                        callback: () => {
-                            this.setState({
-                                showHours: true
-                            });
-                        }
-                    }]}/>
-                    <View style={[Styles.TabIcon]}>
-                        <TouchableOpacity onPress={() => {
-                            openMap({ query: `${this.state.focused.name} ${this.state.focused.location.city}` });
-                        }} style={[{
-                            alignItems: 'center',
-
-                        }]}>
-                            <Entypo name={'map'} size={24}/>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <View>
-                    {(!this.state.showAllInfo && !this.state.showHours) && <Head day={this.state.day} setDay={this._setDay}/>}
-                </View>
-                <View style={[Styles.ModalContent]}>
-                    {this.state.showHours ? <Hours hours={this.state.focused.hours}/> : <View style={[{
-                        flex: 1
-                    }]}><Deals deals={this.state.focused.deals} showAllDeals={this.state.showAllInfo ? true : false} location={this.state.focused} day={this.state.day}/></View>}
-                </View>
+                {this.state.errors.length > 0 && <Text>error</Text>}
             </View>
         )
     }
