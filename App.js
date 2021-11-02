c4dd861d096228af03ac943aaa70299e7e2642e9
Maxx730 <max.kinghorn@gmail.com>
2021-11-02 12:49:27 -0400

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,62 +1,21 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import { Analytics, Event } from "expo-analytics";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const analytics = new Analytics("UA-156478860-1");
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
-//Import Views
-import Main from "./views/Main";
-import Settings from "./views/Settings";
-import EULA from "./views/EULA";
-import Special from "./views/Special";
-import Submit from "./views/Submit";
-import Details from './views/Details';
-
-//Import Data
-import data from "./lib/Data";
-
-const AppNavigator = createStackNavigator({
-  HometownSpecials: {
-    screen: Main,
-    params: {
-      data: data,
-      analytics: analytics
-    },
-    navigationOptions: {
-      headerShown: false
-    }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  Settings: {
-    screen: Settings,
-    params: {
-      analytics: analytics
-    }
-  },
-  EULA: {
-    screen: EULA,
-    params: {
-      analytics: analytics
-    }
-  },
-  Special: {
-    screen: Special,
-    params: {
-      analytics: analytics
-    }
-  },
-  Submit: {
-    screen: Submit,
-    params: {
-
-    }
-  },
-  Details: {
-    screen: Details,
-    params: {
-      analytics: analytics
-    }
-  }
 });
-
-export default createAppContainer(AppNavigator);
-
